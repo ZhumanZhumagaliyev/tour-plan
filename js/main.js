13091b708b25266269e804d4d76ea3f6145c53ed
ZhumanZhumagaliyev <zhuman.zhumagaliyev@gmail.com>
2020-11-26 17:52:10 +0600

    create: reviews section and and adaptive from 320 to 1440

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.hotel-slider', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -13,5 +13,25 @@ var mySwiper = new Swiper('.hotel-slider', {
     onlyInViewport: false,
   },
 
+
   effect: "coverflow",
-})
+}); 
+
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  autoHeight: true,
+}); 
